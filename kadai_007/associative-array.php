32c6598645558f4a1503d5e5dc6a386655e20b53
hiroki0618 <hiro54895@gmail.com>
2024-11-15 14:29:39 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+
+</head>
+
+<body>
+  <p>
+    <?php
+    $Arry=['name'=>'onion','price'=>200,'weight'=>160];
+
+  print_r($Arry);
